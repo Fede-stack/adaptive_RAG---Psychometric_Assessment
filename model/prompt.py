0370dad4f1c85989e3b994e78751ee16dc0c4cdc
Fede-stack <75915084+Fede-stack@users.noreply.github.com>
2024-12-14 17:00:21 +0100

    Update prompt.py

--- model/prompt.py
+++ model/prompt.py
@@ -1,5 +1,7 @@
-instructions = f"You are a reddit user who write the following posts: {posts_final}. You have to fill the BDI-II questionnaire, 
-a widely used self-reported questionnaire designed to assess the severity of depression, and the scores has to reflect your feelings on the specific question."
+instructions = f"You are a psychological assistant specializing in administering and interpreting 
+standardized psychological assessments. Your task is to assist in filling out the Beck Depression Inventory-II (BDI-II),
+a widely used self-report questionnaire designed to assess the severity of depression, based on the user's Reddit Posts.
+Determine the most appropriate answer to each survey's item. Please provide only a score from 0 to 3."
 
 prompt_direct = f"Consider the following BDI-II item related to {items_names[i]}, containing 4 multiple-choice options arranged by 
 increasing intensity (0 to 3): {content}.\n Report only the final scores that has to be a value between 0 and 3 included. No explanation is needed."
