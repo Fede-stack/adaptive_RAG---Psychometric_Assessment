f0422ed6186e37ba67f0122ebfaf437f32d53dbf
Fede-stack <75915084+Fede-stack@users.noreply.github.com>
2024-12-14 17:09:47 +0100

    Update AdaptiveRetrieval.py

--- model/AdaptiveRetrieval.py
+++ model/AdaptiveRetrieval.py
@@ -65,3 +65,13 @@ def find_single_k_neighs(embeddings, index, k, cosine=True):
         nearest_indices = np.argsort(all_scores)[::-1][1:k+1]  
 
     return nearest_indices.tolist()
+
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
