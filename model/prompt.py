e04c4344327d70aec1d31483bd9e55d8bcbc35b7
Fede-stack <75915084+Fede-stack@users.noreply.github.com>
2025-01-01 22:52:17 +0100

    Update prompt.py

--- model/prompt.py
+++ model/prompt.py
@@ -16,6 +16,5 @@ Step 1: Consider the following Reddit posts: {posts_final}. \n
 Step 2: Identify which posts are the most relevant for answering a question related to {items_names[i]}."
 Step 3: Based on the relevant Reddit posts, choose which of the following choices seems most appropriate as a response: {content}. Why does this choice stand out 
 as the best match given the user's current psychological state? Explain the reasoning behind this choice step by step. \n
-Step 4: Finally, report the final score (0-3) based on the intensity of {content}. Use the reasoning from the previous steps to justify your scoring. \n
-Please remember that you are forced to respond with ONLY one of the following values: {[0, 1, 2, 3]}. No explanation is needed.
+Step 4: Finally, report the final score (0-3) based on the intensity of {content}. Use the reasoning from the previous steps to justify your scoring. 
 """
